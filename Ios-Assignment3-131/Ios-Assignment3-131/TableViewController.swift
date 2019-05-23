aa15a557e348963e4835cec1833e319432c286fb
Haochao Zhu <zhuhaochao@gmail.com>
2019-05-23 12:52:42 +1000

    news view prototype completed

--- Ios-Assignment3-131/Ios-Assignment3-131/TableViewController.swift
+++ Ios-Assignment3-131/Ios-Assignment3-131/TableViewController.swift
@@ -37,7 +37,5 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.count
     }
-
-    
 }
 
