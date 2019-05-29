f1d5661e4affcdf72856cc1eaf1288ab4fee2d2d
Julius Sky <julius.skye@gmail.com>
2019-05-30 00:54:58 +1000

    add crypto data to treemap
    add color for each cryptocurrency
    add label for each cryptocurrency
    add datalabel (formatted marketcap value) for each cryptocurrency

--- Ios-Assignment3-131/Ios-Assignment3-131/CryptoTreeMapViewController.swift
+++ Ios-Assignment3-131/Ios-Assignment3-131/CryptoTreeMapViewController.swift
@@ -21,6 +21,17 @@ import UIKit
 
 class CryptoTreeMapViewController: UIViewController {
     
+    // crypto data is market cap data from www.coinmarketcap.com
+    let cryptoMarketCapData = [153226925350.0,
+                               28623380706.0,
+                               18663205894.0,
+                               7898936734.0,
+                               7311677589.0,
+                               7137427929.0,
+                               4763572121.0,
+                               3276030485,
+                               3134341006]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,9 +40,11 @@ class CryptoTreeMapViewController: UIViewController {
         
         let screenSize: CGRect = UIScreen.main.bounds
         
-        // crypto data is market cap data from www.coinmarketcap.com
-        let cryptoData = [153226925350.0,28623380706.0,18663205894.0,7898936734.0,7311677589.0,7137427929.0,4763572121.0]
-        var coords = treeMap.treemapCoords(data: cryptoData,width: Double(screenSize.width),height: Double(screenSize.height),xOffset: 0,yOffset: 0)
+        
+        
+        // make coords of treemap from crypto data
+        var coords = treeMap.treemapCoords(data: cryptoMarketCapData,width: Double(screenSize.width),height: Double(screenSize.height),xOffset: 0,yOffset: 0)
+        
         
         drawTreeMap(coordinates: coords)
         print("SOLUTION \(coords)")
@@ -46,7 +59,15 @@ class CryptoTreeMapViewController: UIViewController {
     }
     
     func drawTreeMap(coordinates: [[Double]]){
+        
+        let cryptoCodes = ["BTC", "ETH", "XRP", "BCC","EOS","LTC","BNC","BSV","USDT"]
+
+        
+        var index = 0
+        var bgColor: CGColor
+
         for coords in coordinates{
+            
             let x1 = coords[0]
             let y1 = coords[1]
             let x2 = coords[2]
@@ -57,8 +78,57 @@ class CryptoTreeMapViewController: UIViewController {
             
             let myView = UIView(frame: CGRect(x: x1, y: y1, width: width, height: height))
             myView.layer.borderWidth = 1
-            myView.layer.borderColor = UIColor.black.cgColor
+            
+            
+//            let uiColors = cryptoData["colors"] as? [String: AnyObject]
+//                (cryptoData.value(forKey: "color")! as AnyObject).valueForKey("BTC")
+//            myView.layer.borderColor = UIColor.black.cgColor
+//            myView.layer.borderColor = uiColors?["BTC"]?.cgColor
+//            myView.layer.borderColor = getCryptoUIColor(cryptoCode: cryptoCodes[index]) as! CGColor
+            bgColor = getCryptoUIColor(cryptoCode: cryptoCodes[index]).cgColor
+            myView.layer.borderColor = bgColor
+            myView.layer.backgroundColor = bgColor
+            
+            let label = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            //label.center = CGPointMake(160, 284)
+            label.textAlignment = NSTextAlignment.center
+            label.text =  cryptoCodes[index]
+            label.adjustsFontSizeToFitWidth = true
+            myView.addSubview(label)
+            
+            let datalabel = UILabel(frame: CGRect(x: 0, y: 50, width: 50, height: 50))
+            datalabel.textAlignment = NSTextAlignment.center
+            datalabel.text =  String(format: "%.2f", cryptoMarketCapData[index]/pow(10, 9))
+            datalabel.adjustsFontSizeToFitWidth = true
+            myView.addSubview(datalabel)
+
+            index+=1
+            
+            
             self.view.addSubview(myView)
         }
     }
+    
+    func getCryptoUIColor(cryptoCode: String)-> UIColor{
+        switch cryptoCode {
+                    case "BTC","BCC":
+                        return UIColor.orange
+                    case "BSV":
+                        return UIColor.yellow
+                    case "ETH":
+                        return UIColor.purple
+                    case "LTC":
+                        return UIColor.gray
+                    case "XRP":
+                        return UIColor.blue
+                    case "EOS":
+                        return UIColor.magenta
+                    case "USDT":
+                        return UIColor.green
+                    default:
+                        return UIColor.white
+                    }
+
+    }
+    
 }
